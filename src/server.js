89d1dc8b4b79b167ed5033d55c86e07e7472ac12
byungsu.jeong <byungsu.jeong88@gmail.com>
2020-09-26 07:46:52 +0900

    build

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import logger from "morgan";
 import socketController from "./socketController";
 import events from "./event";
 
-const PORT = process.env.port || 4000;
+const PORT = 5000 || 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
