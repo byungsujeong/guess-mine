5186351b6252d295614d9bda2340a416595d6fba
byungsu.jeong <byungsu.jeong88@gmail.com>
2020-09-26 07:52:39 +0900

    build

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import logger from "morgan";
 import socketController from "./socketController";
 import events from "./event";
 
-const PORT = process.env.port || 4000;
+//const PORT = process.env.PORT || 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
@@ -16,7 +16,7 @@ app.get("/", (req, res) => res.render("home", { events: JSON.stringify(events) }
 //const handleListening = () => console.log(`✅ Server running: http://localhost:${PORT}`);
 const handleListening = () => console.log(`✅ Server running: https://safe-thicket-91758.herokuapp.com/`);
 
-const server = app.listen(PORT, handleListening);
+const server = app.listen(process.env.PORT || 4000, handleListening);
 
 const io = socketIO.listen(server);
 
